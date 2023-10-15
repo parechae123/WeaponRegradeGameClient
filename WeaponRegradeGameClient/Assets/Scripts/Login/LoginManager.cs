fc8bb9efdbe7a293f5347f0667086d447d5108ef
heoyoon <parechae@gmail.com>
2023-10-15 23:38:38 +0900

    feat : 회원가입 함수 기본만 만들어둠

--- WeaponRegradeGameClient/Assets/Scripts/Login/LoginManager.cs
+++ WeaponRegradeGameClient/Assets/Scripts/Login/LoginManager.cs
@@ -7,18 +7,50 @@ using UnityEngine;
 using TMPro;
 public class LoginManager : MonoBehaviour
 {
+    [Header("로그인")]
     public TMP_InputField usernameInput;
     public TMP_InputField passwordInput;
     public TextMeshProUGUI loginStatusText;
+    [Header("회원가입")]
+    public TMP_InputField usernameRegist;
+    public TMP_InputField passwordRegist;
+    public TextMeshProUGUI loginRegist;
 
     public string token;
 
     //아래 URL은 응용시 본인 호스트 주소로 변경행줘야함
     private const string apiUrl = "http://localhost:3000";  //Node.js 주소
-
+    public void Regist()
+    {
+        StartCoroutine(AttemptRegist(usernameRegist.text,passwordRegist.text));
+    }
     public void Login()
     {
         StartCoroutine(AttemptLogin(usernameInput.text, passwordInput.text));
+    }
+    IEnumerator AttemptRegist(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(apiUrl + "/resgist", form))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                loginStatusText.text = "Login failed : " + webRequest.error;
+            }
+            else
+            {
+                loginStatusText.text = "Login successful!";
+                string responseText = webRequest.downloadHandler.text;
+                //JSON 응답에서 토큰 값을 추출
+                var responseData = JsonConvert.DeserializeObject<ResponseData>(responseText);
+                token = responseData.token;
+
+                Debug.Log("Login successful! Token : " + token);
+            }
+        }
     }
     IEnumerator AttemptLogin(string username,string password)
     {
