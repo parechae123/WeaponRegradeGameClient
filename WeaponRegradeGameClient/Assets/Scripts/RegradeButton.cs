5da88b8f5866630c19fedb2bb5181e87a0042c39
HS010315 <hspower01@gmail.com>
2023-11-26 19:24:24 +0900

    Feat : "강화/판매 시 데이터 업데이트 기능"

--- WeaponRegradeGameClient/Assets/Scripts/RegradeButton.cs
+++ WeaponRegradeGameClient/Assets/Scripts/RegradeButton.cs
@@ -1,10 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
 public class RegradeButton : MonoBehaviour
 {
-    private GameManager gameManager;
+    public GameManager gameManager;
 
     private void Awake()
     {
@@ -22,6 +23,12 @@ public class RegradeButton : MonoBehaviour
         {
             UIDataManager.Instance.changeNowWeapon(1);
         }
+        string userID = gameManager.userValue.userID;
+        string money = gameManager.PlayerInven.money.ToString();
+        string maxRegrade = gameManager.PlayerInven.maxRegrade.ToString();
+        string weaponIndex = gameManager.PlayerInven.WeaponIndex.ToString();
+
+        StartCoroutine(SendDataToServer(userID, money, maxRegrade, weaponIndex));
     }
 
     private bool isRegradeSucces()
@@ -34,6 +41,28 @@ public class RegradeButton : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+    IEnumerator SendDataToServer(string userID, string money, string maxRegrade, string weaponIndex)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("userID", userID);
+        form.AddField("money", money);
+        form.AddField("maxRegrade", maxRegrade);
+        form.AddField("WeaponIndex", weaponIndex);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(gameManager.apiUrl + "/updateInven", form))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Failed to save data to the server: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Data saved successfully!");
+            }
         }
     }
 }
--- WeaponRegradeGameClient/Assets/Scripts/SellButton.cs
+++ WeaponRegradeGameClient/Assets/Scripts/SellButton.cs
@@ -1,10 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
 public class SellButton : MonoBehaviour
 {
-    private GameManager gameManager;
+    public GameManager gameManager;
     private void Awake()
     {
         gameManager = GameObject.Find("Managers").GetComponent<GameManager>();
@@ -16,6 +17,35 @@ public class SellButton : MonoBehaviour
         {
             GameManager.Instance.playerInven.money += UIDataManager.Instance.nowWeapon.sellValue;
             UIDataManager.Instance.changeNowWeapon(1);
+        }
+
+        string userID = gameManager.userValue.userID;
+        string money = gameManager.PlayerInven.money.ToString();
+        string maxRegrade = gameManager.PlayerInven.maxRegrade.ToString();
+        string weaponIndex = gameManager.PlayerInven.WeaponIndex.ToString();
+
+        StartCoroutine(SendDataToServer(userID, money, maxRegrade, weaponIndex));
+    }
+    IEnumerator SendDataToServer(string userID, string money, string maxRegrade, string weaponIndex)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("userID", userID);
+        form.AddField("money", money);
+        form.AddField("maxRegrade", maxRegrade);
+        form.AddField("WeaponIndex", weaponIndex);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(gameManager.apiUrl + "/updateInven", form))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Failed to save data to the server: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Data saved successfully!");
+            }
         }
     }
 }
